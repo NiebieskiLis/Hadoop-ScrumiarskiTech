584dcb4255f2ac4d273b06b71a9b6a312b4374ae
NiebieskiLis <40572595+NiebieskiLis@users.noreply.github.com>
2019-10-27 21:36:55 +0100

    Add files via upload

---
+++ Komendy (1).sql
@@ -0,0 +1,26 @@
+use Hadoop;
+create table Firma 
+(ID_Firma int, 
+Nazwa_Frimy nvarchar(50),
+Sektor_Gospodarki nvarchar(50)) 
+create table Ludzie (ID_Osoby int, Imie_i_nazwisko nvarchar(50), Numer_pracownika int)  ;
+create table Typ (ID_Typu int, Typ nvarchar(50))  ;
+create table Status (ID_Status int, Status nvarchar(50))  ;
+create table Data (ID_Daty int, Data Date, Rok Int, Miesiac int, Dzien int )  ;
+
+create table Produkt (ID_Produkt int, ID_Product_Owner int, nazwa nvarchar(50))  ;
+
+--create table Produkt (ID_Produkt int, ID_Product_Owner int, nazwa nvarchar(50), inne MAP<nvarchar(50),nvarchar(50)>) 
+--ROW FORMAT DELIMITED 
+--FIELDS TERMINATED BY ',' 
+--COLLECTION ITEMS TERMINATED BY '#'
+--MAP KEYS TERMINATED BY '@'
+--STORED AS TEXTFILE;
+--LOAD DATA INPATH '/tmp/data/SampleData3.txt' OVERWRITE INTO TABLE Produkt;
+--drop table dbo.Wykonanie_zadania
+create  table Product_Backlog (ID_Product_Backlog int, ID_Product int, ID_Deadline int, ID_Poczatek int, ID_Koniec int, ID_Firmy int, Koszt float, Cena float, Profit float, liczba_zadan int)  ;
+create  table Sprint (ID_Sprint int,ID_Product_Backlog int, ID_Poczatek int, ID_Product int, ID_Koniec int, ID_Scrum_Master int, Planowana_objetosc float, Rzeczywista_objetosc float, Procent_wykonania float, Liczba_bledow_na_wdrozenie int, Planowana_liczba_ceremonii int, Finalna_liczba_ceremonii int)  ;
+create  table Wykonanie_zadania (ID_Wykonanie_zadania int, ID_Sprintu int, ID_Status int, ID_Zadania int,ID_Typ int, Czas_wykonania float, Estymacja_zadania float, Liczba_committow int, Liczba_zmienionych_plikow int, Liczba_zmienionych_linijek_kodu int)  ;
+
+
+
--- ZapytaniaSQL.sql
+++ ZapytaniaSQL.sql
@@ -12,3 +12,51 @@ where PB1.ID_Product_Backlog!= PB2.ID_Product_Backlog
 and PB1.ID_Poczatek > PB2.ID_Poczatek
 and PB1.liczba_zadan>PB2.liczba_zadan
 
+--2. 
+--Sektor gospodarczy pobieramy z tabeli DIM_Firma. Nastêpnie sprawdzamy
+--czy zdarzy³o siê przekroczenie objêtoœci planowanej dla sprintu [Tabela
+--FACT_Sprint] gdzie procent wykonania musia³by byæ wiêkszy ni¿ 100
+
+select F.Sektor_Gospodarki , S.ID_Sprint from Sprint as S
+join [dbo].[Product_Backlog] as PB on S.[ID_Product_Backlog]=PB.ID_Product_Backlog
+join [dbo].[Firma] as F on F.ID_Firma = PB.ID_Firmy 
+where [Rzeczywista_objetosc]>[Planowana_objetosc]
+or [Procent_wykonania]>100
+--3.
+--Filtrujemy sprinty po Scrummasterach korzystaj¹c z ID_Scrum_Master,,
+--nastêpnie dla ka¿dego z nich wyznaczamy œredni¹ liczbê b³êdów na wdro¿enie
+--ze wszystkich przeprowadzonych przez nich sprintów, sortujemy ich po
+--œredniej rosn¹co i wyœwietlamy 3 pozycje.
+
+select l.Imie_i_nazwisko ,l.Numer_pracownika ,AVG(s.Liczba_bledow_na_wdrozenie) as [srednia liczba bledow na wdrozenie]  from [dbo].[Sprint] as s
+join [dbo].[Ludzie] as l on s.ID_Scrum_Master=l.ID_Osoby
+group by  l.Imie_i_nazwisko ,l.Numer_pracownika
+order by  AVG(s.Liczba_bledow_na_wdrozenie)
+
+--4. Filtrujemy zadania po ich typie korzystaj¹c z tabeli wymiarów Typ oraz
+--ID_typu w tabeli faktu i wybieramy tylko te typu frontendowego, a nastêpnie
+--na podstawie ró¿nicy miêdzy estymowanym a rzeczywistym czasem
+--wykonania kazdego zadania wyliczamy ich œredni¹.
+
+select AVG(W.[Estymacja_zadania]-W.[Czas_wykonania]), W.ID_Zadania from [dbo].[Wykonanie_zadania] as W
+join Typ as T on T.ID_Typu=W.ID_Typ
+where T.Typ = 'Fortend'
+group by W.ID_Zadania
+
+--5. Filtrujemy zadania po ich typie korzystaj¹c z tabeli wymiarów Typ oraz
+--ID_typu w tabeli faktu, dla typów frontendowego i backendowego obliczamy
+--œredni¹ z ro¿nicy rzeczywistego oraz estymowanego czasu wykonania i je
+--porównujemy
+--6. Datê oraz profit pobieramy z tabeli FACT Product Backlog.
+--7. Sumujemy iloœæ commitów dla ka¿dego z pracowników oraz dzielimy je w
+--stosunku do daty zawartej jako klucz obcy w tabeli FACT Sprint (ID_Koniec)
+--8. Sumujemy status “nie wykonany” z tabeli wymiaru DIM_Status na przeciêciu
+--z tabel¹ DIM_Typ
+select COUNT(*) from [dbo].[Wykonanie_zadania] as W
+join [dbo].[Status] as T on T.ID_Status = W.ID_Status
+where T.Status = 'Nieukonczone'
+
+--9. Sumujemy profit z Tabeli FACT_Product_Backlog oraz dzielimy go na liczbê
+--dni miêdzy pocz¹tkiem a zakoñczeniem Product Backlogu.
+--10.Sektor Gospodarki znajdziemy w tabeli DIM_Firma a koszty produktów w
+--FACT Product Backlog.
