7fcefbd240f8eef0fbdef1b7f740495670f8e9bd
NiebieskiLis <40572595+NiebieskiLis@users.noreply.github.com>
2019-10-27 20:09:16 +0100

    zapytanie 1 do sprawdzania

--- ZapytaniaSQL.sql
+++ ZapytaniaSQL.sql
@@ -1,10 +1,14 @@
 --zapytania 
 --1 
---Pobieramy wszystkie product backlogi dla danego produktu (ograniczajÄ…c po
+--Pobieramy wszystkie product backlogi dla danego produktu (ograniczaj¹c po
 --ID_produktu z tabeli faktu) i sprawdzamy czas wraz z aktualizacjami
---backlogÃ³w liczba zadaÅ„ i tym samym zÅ‚oÅ¼onoÅ›Ä‡ projektu siÄ™ zwiÄ™ksza na
---podstawie miary Liczba_zadaÅ„
-select PB1.ID_Poczatek , PB2.ID_Poczatek from Product_backlog as PB1
+--backlogów liczba zadañ i tym samym z³o¿onoœæ projektu siê zwiêksza na
+--podstawie miary Liczba_zadañ
+--PB1 to ten nowszy
+select PB1.ID_Poczatek , PB2.ID_Poczatek , P.nazwa from Product_backlog as PB1
 join Produkt as P on PB1.ID_product=P.ID_produkt
 join Product_backlog as PB2 on PB2.ID_product=P.ID_produkt
 where PB1.ID_Product_Backlog!= PB2.ID_Product_Backlog
+and PB1.ID_Poczatek > PB2.ID_Poczatek
+and PB1.liczba_zadan>PB2.liczba_zadan
+
