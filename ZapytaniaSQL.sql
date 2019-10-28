50bdb8db2782b65af6ef482c5d4ed0d121012729
NiebieskiLis <40572595+NiebieskiLis@users.noreply.github.com>
2019-10-28 23:12:03 +0100

    Update ZapytaniaSQL.sql

--- ZapytaniaSQL.sql
+++ ZapytaniaSQL.sql
@@ -1,9 +1,9 @@
 --zapytania 
 --1 
---Pobieramy wszystkie product backlogi dla danego produktu (ograniczajπc po
+--Pobieramy wszystkie product backlogi dla danego produktu (ograniczajƒÖc po
 --ID_produktu z tabeli faktu) i sprawdzamy czas wraz z aktualizacjami
---backlogÛw liczba zadaÒ i tym samym z≥oøonoúÊ projektu siÍ zwiÍksza na
---podstawie miary Liczba_zadaÒ
+--backlog√≥w liczba zada≈Ñ i tym samym z≈Ço≈ºono≈õƒá projektu siƒô zwiƒôksza na
+--podstawie miary Liczba_zada≈Ñ
 --PB1 to ten nowszy
 select PB1.ID_Poczatek , PB2.ID_Poczatek , P.nazwa from Product_backlog as PB1
 join Produkt as P on PB1.ID_product=P.ID_produkt
@@ -13,9 +13,9 @@ and PB1.ID_Poczatek > PB2.ID_Poczatek
 and PB1.liczba_zadan>PB2.liczba_zadan
 
 --2. 
---Sektor gospodarczy pobieramy z tabeli DIM_Firma. NastÍpnie sprawdzamy
---czy zdarzy≥o siÍ przekroczenie objÍtoúci planowanej dla sprintu [Tabela
---FACT_Sprint] gdzie procent wykonania musia≥by byÊ wiÍkszy niø 100
+--Sektor gospodarczy pobieramy z tabeli DIM_Firma. Nastƒôpnie sprawdzamy
+--czy zdarzy≈Ço siƒô przekroczenie objƒôto≈õci planowanej dla sprintu [Tabela
+--FACT_Sprint] gdzie procent wykonania musia≈Çby byƒá wiƒôkszy ni≈º 100
 
 select F.Sektor_Gospodarki , S.ID_Sprint from Sprint as S
 join [dbo].[Product_Backlog] as PB on S.[ID_Product_Backlog]=PB.ID_Product_Backlog
@@ -23,39 +23,52 @@ join [dbo].[Firma] as F on F.ID_Firma = PB.ID_Firmy
 where [Rzeczywista_objetosc]>[Planowana_objetosc]
 or [Procent_wykonania]>100
 --3.
---Filtrujemy sprinty po Scrummasterach korzystajπc z ID_Scrum_Master,,
---nastÍpnie dla kaødego z nich wyznaczamy úredniπ liczbÍ b≥ÍdÛw na wdroøenie
---ze wszystkich przeprowadzonych przez nich sprintÛw, sortujemy ich po
---úredniej rosnπco i wyúwietlamy 3 pozycje.
+--Filtrujemy sprinty po Scrummasterach korzystajƒÖc z ID_Scrum_Master,,
+--nastƒôpnie dla ka≈ºdego z nich wyznaczamy ≈õredniƒÖ liczbƒô b≈Çƒôd√≥w na wdro≈ºenie
+--ze wszystkich przeprowadzonych przez nich sprint√≥w, sortujemy ich po
+--≈õredniej rosnƒÖco i wy≈õwietlamy 3 pozycje.
 
 select TOP(3) l.Imie_i_nazwisko ,l.Numer_pracownika ,AVG(s.Liczba_bledow_na_wdrozenie) as [srednia liczba bledow na wdrozenie]  from [dbo].[Sprint] as s
 join [dbo].[Ludzie] as l on s.ID_Scrum_Master=l.ID_Osoby
 group by  l.Imie_i_nazwisko ,l.Numer_pracownika
 order by  AVG(s.Liczba_bledow_na_wdrozenie)
 
---4. Filtrujemy zadania po ich typie korzystajπc z tabeli wymiarÛw Typ oraz
---ID_typu w tabeli faktu i wybieramy tylko te typu frontendowego, a nastÍpnie
---na podstawie rÛønicy miÍdzy estymowanym a rzeczywistym czasem
---wykonania kazdego zadania wyliczamy ich úredniπ.
+--4. Filtrujemy zadania po ich typie korzystajƒÖc z tabeli wymiar√≥w Typ oraz
+--ID_typu w tabeli faktu i wybieramy tylko te typu frontendowego, a nastƒôpnie
+--na podstawie r√≥≈ºnicy miƒôdzy estymowanym a rzeczywistym czasem
+--wykonania kazdego zadania wyliczamy ich ≈õredniƒÖ.
 
 select AVG(W.[Estymacja_zadania]-W.[Czas_wykonania]), W.ID_Zadania from [dbo].[Wykonanie_zadania] as W
 join Typ as T on T.ID_Typu=W.ID_Typ
 where T.Typ = 'Fortend'
 group by W.ID_Zadania
 
---5. Filtrujemy zadania po ich typie korzystajπc z tabeli wymiarÛw Typ oraz
---ID_typu w tabeli faktu, dla typÛw frontendowego i backendowego obliczamy
---úredniπ z roønicy rzeczywistego oraz estymowanego czasu wykonania i je
---porÛwnujemy
+--5. Filtrujemy zadania po ich typie korzystajƒÖc z tabeli wymiar√≥w Typ oraz
+--ID_typu w tabeli faktu, dla typ√≥w frontendowego i backendowego obliczamy
+--≈õredniƒÖ z ro≈ºnicy rzeczywistego oraz estymowanego czasu wykonania i je
+--por√≥wnujemy
 select AVG(W.[Estymacja_zadania]-W.[Czas_wykonania]),T.Typ from [dbo].[Wykonanie_zadania] as W
 join Typ as T on T.ID_Typu=W.ID_Typ
 group by T.Typ
 
---Podaj mi zaleønoúÊ profitu z zakoÒczonych produktÛw w danym miesiπcu w
---porÛwnaniu do zesz≥ego roku.(3)
---6. DatÍ oraz profit pobieramy z tabeli FACT Product Backlog.
---Podaj mi pracownika, ktÛry wykona≥ najwiÍkszπ iloúÊ commitÛw wg miesiÍcy
---(4)
+--Podaj mi zale≈ºno≈õƒá profitu z zako≈Ñczonych produkt√≥w w danym miesiƒÖcu w
+--por√≥wnaniu do zesz≈Çego roku.(3)
+--6. Datƒô oraz profit pobieramy z tabeli FACT Product Backlog.
+--Podaj mi pracownika, kt√≥ry wykona≈Ç najwiƒôkszƒÖ ilo≈õƒá commit√≥w wg miesiƒôcy
+--(4)
+create view widok as
+  SELECT MAX(W1.Liczba_committow) AS [max  number of commits], D.Rok, D.Miesiac, L.Imie_i_nazwisko
+FROM     dbo.Ludzie AS L INNER JOIN
+                  dbo.Wykonanie_zadania AS W1 ON L.ID_Osoby = W1.ID_osoby INNER JOIN
+                  dbo.Sprint AS S ON S.ID_Sprint = W1.ID_Sprintu INNER JOIN
+                  dbo.Data AS D ON D.ID_Daty = S.ID_Poczatek
+GROUP BY D.Rok, D.Miesiac, L.Imie_i_nazwisko
+  select Widok.Imie_i_nazwisko ,[max  number of commits] , rok_max.Rok , rok_max.Miesiac from (select max([max  number of commits]) as maks ,[Rok],[Miesiac]
+  from [Hadoop].[dbo].[widok] group by  [Rok],[Miesiac])  rok_max , widok
+  where widok.[max  number of commits] = rok_max.maks and rok_max.Miesiac=widok.Miesiac and rok_max.Rok=widok.Rok
+                                               
+                                               
+                                               
 select max(W1.Liczba_committow) as [max  number of commits] , D.Rok ,D.Miesiac,L.Imie_i_nazwisko from  [dbo].[Ludzie] as L
 join [dbo].[Wykonanie_zadania] as W1 on L.ID_Osoby=W1.ID_osoby
 join [dbo].[Sprint] as S on S.ID_Sprint=W1.ID_Sprintu
@@ -70,14 +83,14 @@ group by D.Rok , D.Miesiac
 select * from B as C
 join B as A on C.Rok  = A.Rok AND A.Miesiac = C.Miesiac-1 
 
---8. Sumujemy status ìnie wykonanyî z tabeli wymiaru DIM_Status na przeciÍciu
---z tabelπ DIM_Typ
+--8. Sumujemy status ‚Äúnie wykonany‚Äù z tabeli wymiaru DIM_Status na przeciƒôciu
+--z tabelƒÖ DIM_Typ
 select COUNT(*) as [ile zadan nie zostalo wykonanych] from [dbo].[Wykonanie_zadania] as W
 join [dbo].[Status] as T on T.ID_Status = W.ID_Status
 where T.Status = 'Nieukonczone'
 
---9. Sumujemy profit z Tabeli FACT_Product_Backlog oraz dzielimy go na liczbÍ
---dni miÍdzy poczπtkiem a zakoÒczeniem Product Backlogu.
+--9. Sumujemy profit z Tabeli FACT_Product_Backlog oraz dzielimy go na liczbƒô
+--dni miƒôdzy poczƒÖtkiem a zako≈Ñczeniem Product Backlogu.
 SELECT TOP(3) P.nazwa, (PB.Profit/((DATEDIFF(DD,D1.Data ,D2.Data )))) as 'Profit na dzien'  from Product_backlog as PB
 join Produkt as P on PB.ID_product=P.ID_produkt
 join Data as D1 on PB.ID_Poczatek=D1.ID_Daty
@@ -87,7 +100,7 @@ where PB.ID_Koniec is not null
 order by 'Profit na dzien'
 
 
---10.Sektor Gospodarki znajdziemy w tabeli DIM_Firma a koszty produktÛw w
+--10.Sektor Gospodarki znajdziemy w tabeli DIM_Firma a koszty produkt√≥w w
 --FACT Product Backlog.
 SELECT SUM(PB.Profit) as profit, F.Sektor_Gospodarki as Sektor_Gospodarki from Product_Backlog as PB
 join [dbo].[Firma] as F on F.ID_Firma = PB.ID_Firmy 
